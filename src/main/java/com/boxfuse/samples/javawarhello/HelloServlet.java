66b006c39190aa2b6f8784610843ea15fc62ab92
devopsmoon <42669876+devopsmoon@users.noreply.github.com>
2019-07-12 16:00:27 +0530

    MPH-1 commit

--- src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
+++ src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(urlPatterns = "/hello", loadOnStartup = 1)
+@WebServlet(urlPatterns = "/heeello", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
